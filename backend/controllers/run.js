795d1ff3d95b77953f1bb3965f1a4792d4be072e
Adnan Alper Demirci <aalperdemirci@gmail.com>
2024-08-11 15:34:16 +0300

    Edit and delete run events

--- backend/controllers/run.js
+++ backend/controllers/run.js
@@ -19,4 +19,43 @@ const createRun = async (req, res) => {
     }
 }
 
-module.exports = { createRun }
+const editRun = async (req, res) => {
+    try {
+        const runId = req.params.id
+        const runner = req.user.userName
+
+        const selectedRun = await runModel.findById(runId)
+        if (selectedRun.runner === runner) {
+            const editedRun = req.body
+            await runModel.findByIdAndUpdate(runId, editedRun)
+            
+            return res.send({ msg: 'Running event is edited.' })
+        } else {
+            return res.send({ msg: 'Wrong author.' })
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ msg: 'Internal server error' })
+    }
+}
+
+const deleteRun = async (req, res) => {
+    try {
+        const runId = req.params.id
+        const runner = req.user.userName
+
+        const selectedRun = await runModel.findById(runId)
+        if (selectedRun.runner === runner) {
+            await runModel.findByIdAndDelete(runId)
+
+            return res.send({ msg: 'Running event is deleted.' })
+        } else {
+            return res.send({ msg: 'Wrong author.' })
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ msg: 'Internal server error' })
+    }
+}
+
+module.exports = { createRun, editRun, deleteRun }
--- backend/routers/run.js
+++ backend/routers/run.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { createRun } = require('../controllers/run')
+const { createRun, editRun, deleteRun } = require('../controllers/run')
 const verifyToken = require('../middleware/auth')
 
 
 router.post('/create', verifyToken, createRun)
+router.put('/edit/:id', verifyToken, editRun)
+router.delete('/delete/:id', verifyToken, deleteRun)
 
 module.exports = router
