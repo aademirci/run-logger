b20d3c0d19fbdf461fc4a80b265e4b41ae792142
Adnan Alper Demirci <aalperdemirci@gmail.com>
2024-08-19 21:45:34 +0300

    Add links in the NavBar

--- client/src/Common/NavBar.jsx
+++ client/src/Common/NavBar.jsx
@@ -1,16 +1,30 @@
+import { useEffect, useState } from "react"
+import { useCookies } from "react-cookie"
+import { Link } from "react-router-dom"
 
 const NavBar = () => {
-  return (
-    <nav className="nav-bar">
-        <h1>RunLogger</h1>
-        <ul className="nav-menu">
-            <li>Leaderboard</li>
-            <li>Search</li>
-            <li><button>Log run</button></li>
-            <li>Profile</li>
-        </ul>
-    </nav>
-  )
+    const [cookies] = useCookies(['runlogger'])
+    const [userName, setUserName] = useState('')
+
+    useEffect(() => {
+        if (cookies.runlogger) {
+            const tokenArray = cookies.runlogger.split('.')
+            const tokenPayload = JSON.parse(window.atob(tokenArray[1]))
+            setUserName(tokenPayload.userName)
+        }
+    }, [cookies])
+
+    return (
+        <nav className="nav-bar">
+            <h1><Link to={`/`}>RunLogger</Link></h1>
+            <ul className="nav-menu">
+                <li>Leaderboard</li>
+                <li>Search</li>
+                <li><Link to={`/user/${userName}/run/create`} className="button">Log run</Link></li>
+                <li><Link to={`/user/${userName}`}>Profile</Link></li>
+            </ul>
+        </nav>
+    )
 }
 
 export default NavBar
